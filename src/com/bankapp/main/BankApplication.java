b543ad859e52642e34691f4e77e6832c24127d51
Sameetha P <sameethaperumal@gmail.com>
2026-01-28 05:52:28 +0530

    Initial project structure with layered architecture

---
+++ src/com/bankapp/main/BankApplication.java
@@ -0,0 +1,8 @@
+package com.bankapp.main;
+
+public class BankApplication {
+    public static void main(String[] args) {
+        System.out.println("Banking Management Application Started!...");
+    }
+}
+
